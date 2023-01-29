bc68e424f30bdacbb751aa27836034570d469ae4
Alex <avdeenko.aleksey@yandex.com>
2023-01-29 06:34:55 +0300

    pre release

--- app/app.py
+++ app/app.py
@@ -1,43 +1,256 @@
-from quart import Quart
-from logger import create_logger
-from quart import request
-from telegram.ext import Updater
-from telegram import Update
-from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+from internal_functions import *
 
-import datetime,time
 log = create_logger('app.log', 'app', 10)
-
-token="5802197857:AAFu-F88ekbr3kvB745HE3Iqt7oEVm6fOAU"
+token = os.environ.get('token')
+commands = ['/category', '/total', '/set_category']
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
-# updater = Updater(token=token, use_context=True)
-# import os
-# from dotenv import load_dotenv, find_dotenv
-# load_dotenv(find_dotenv())
-# SECRET_KEY = os.environ.get("SECRET_KEY")
-# DATABASE_PASSWORD = os.environ.get("DATABASE_PASSWORD")
-
-# app = Quart(__name__)
+    global commands
+    log.debug(f'chat_id: {update.effective_chat.id}, user_id {update.effective_user.id}')
+    commands = "\n".join(commands)
+    await update.message.reply_text(f'Добро пожаловать в тестовый бот. Список команд: \n{commands}')
+    await get_user_db_id(update, context)
+    await create_daily_job(update, context)
 
 
-# @app.route("/",methods=["POST"])
-# async def webhook():
-#     data = await request.get_json()
-#     log.debug(f'good: {data}')
-#     # with open('fuck','w') as f:
-#     #     f.write(f'{datetime.datetime.now()}: {data}')
-#     return {"input": data, "extra": True}
+@checking()
+async def get_categories(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    user_id = context.user_data.get('user_db_id')
+    db.execute(
+        "SELECT users_categories.id,categories.name FROM users_categories JOIN categories on categories.id = users_categories.category_id  WHERE  users_categories.user_id = %s",
+        (user_id,))
+    user_categories = db.fetchall()
+    if not user_categories:
+        await update.message.reply_text("У вас нет добавленых категорий \n:( Добавьте их через команду \n/set_category")
+        return
+    user_categories = sorted(user_categories, key=lambda x: x[1])
+    keys = [[InlineKeyboardButton(text=f"{name}", callback_data=f"category_{id}_{name}")] for id, name in
+            user_categories]
+    markup = InlineKeyboardMarkup(keys)
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="Выберите категорию", reply_markup=markup)
+
+
+@checking()
+async def get_total(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    db.execute(
+        "SELECT ue.amount FROM users_expenses as ue JOIN users_categories as uc ON ue.user_category_id=uc.id  WHERE  uc.user_id = %s",
+        (context.user_data.get('user_db_id'),))
+    exps = db.fetchall()
+    if exps:
+        total = sum([x[0] for x in exps])
+        text = f"Общая сумма расходов составляет {total}"
+    else:
+        text = f"У вас пока нет расходов. Добавьте их через категории"
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=text)
+
+
+@checking()
+async def set_category(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.effective_message.reply_text("Введите название категории")
+    context.user_data['action'] = 'setting_category'
+
+
+@checking()
+async def add_category(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    category = str(context.user_data.get('setting_category')).capitalize().strip()
+    user_id = context.user_data.get('user_db_id')
+    db.execute("SELECT id,name FROM categories WHERE  name = %s", (category,))
+    cat = db.fetchone()
+    if not cat:
+        db.execute("INSERT INTO categories (name) VALUES (%s) RETURNING id", (category,))
+        db_connection.commit()
+        cat_id = db.fetchone()[0]
+    else:
+        cat_id = cat[0]
+    db.execute("SELECT id FROM users_categories WHERE  user_id = %s and category_id = %s", (user_id, cat_id,))
+    if db.fetchone():
+        text = f'Категория \"{category}\" у вас уже есть'
+        await context.bot.send_message(chat_id=update.effective_chat.id, text=text)
+        return
+    db.execute("INSERT INTO users_categories (user_id,category_id) VALUES (%s,%s)", (user_id, cat_id,))
+    db_connection.commit()
+    text = f'Категория \"{category}\" успешно добавлена'
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=text)
+    context.user_data['action'] = 0
+
+
+async def enter_date_caption(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    markup = InlineKeyboardMarkup([
+        [InlineKeyboardButton(text="Сегодня", callback_data=f"date_today")],
+        [InlineKeyboardButton(text="Вчера", callback_data=f"date_yestarday")],
+        [InlineKeyboardButton(text="Позавчера", callback_data=f"date_preyestarday")],
+        [InlineKeyboardButton(text="Ввести вручную", callback_data=f"date_write")],
+        [InlineKeyboardButton(text="Назад", callback_data=f"date_back")]
+    ])
+    text = f"Категория {context.user_data.get('choosen_category_name')}, выберете дату"
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=text, reply_markup=markup)
+
+
+async def write_date_caption(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    text = f"Введите дату в формате ДД.ММ.ГГ"
+    markup = InlineKeyboardMarkup([[InlineKeyboardButton(text="Назад", callback_data=f"add_expenses_back")]])
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=text, reply_markup=markup)
+
+
+async def write_date(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    date = str(context.user_data.get('writed_date')).replace(',', '.').replace(' ', '.')
+    log.debug(f'date: {date}')
+    try:
+        date = datetime.datetime.strptime(date, '%d.%m.%y').date()
+    except:
+        text = "Ошибка! Дата введена некорректно. Исправьте и отправьте сообщение повторно."
+        await context.bot.send_message(chat_id=update.effective_chat.id, text=text)
+        return
+    context.user_data['expenses_date'] = date
+    await show_expenses(update, context)
+
+
+async def delete_expenses_caption(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    text = "Выберете пункты для удаления"
+    date = context.user_data.get('expenses_date')
+    cat_id = context.user_data.get('choosen_category_id')
+    db.execute("SELECT id,name,amount FROM users_expenses  WHERE  user_category_id = %s and date = %s", (cat_id, date,))
+    exps = db.fetchall()
+    log.debug(f'date: {date}, cat_id: {cat_id}')
+    markup = []
+    for id, name, amount in exps:
+        caption = f"{name} - {amount if int(amount) != amount else int(amount)}"
+        markup.append([InlineKeyboardButton(text=caption, callback_data=f"del_exp_{id}")])
+    markup.append([InlineKeyboardButton(text="Назад", callback_data=f"add_expenses_back")])
+    markup = InlineKeyboardMarkup(markup)
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=text, reply_markup=markup)
+
+
+async def add_expenses_caption(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    text = "Введите название траты и сумму через пробел (можно несколько строк)"
+    markup = InlineKeyboardMarkup([[InlineKeyboardButton(text="Назад", callback_data=f"write_expenses_back")]])
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=text, reply_markup=markup)
+
+
+async def add_expenses(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    exps = str(context.user_data.get('writed_expenses')).split('\n')
+    for exp in exps:
+        if ' ' in exp:
+            name, amount = exp[:exp.rindex(' ')], exp[exp.rindex(' ') + 1:]
+            try:
+                amount = float(amount.replace(',', '.'))
+            except:
+                text = "Ошибка! Сумма введена некорректно. Исправьте и отправьте сообщение повторно."
+                await context.bot.send_message(chat_id=update.effective_chat.id, text=text)
+                return
+            db.execute("INSERT INTO users_expenses (name,amount,user_category_id,date) VALUES (%s, %s, %s, %s)",
+                       (name, amount, context.user_data['choosen_category_id'], context.user_data['expenses_date'],))
+            db_connection.commit()
+        else:
+            text = "Ошибка! Данные введены некорректно. Исправьте и отправьте сообщение повторно."
+            await context.bot.send_message(chat_id=update.effective_chat.id, text=text)
+            return
+    context.user_data['action'] = 'show_expenses'
+    await show_expenses(update, context)
+
+
+@checking()
+async def show_expenses(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    date = context.user_data.get('expenses_date')
+    user_id = context.user_data.get('user_db_id')
+    cat_id = context.user_data.get('choosen_category_id')
+    markup = [[InlineKeyboardButton(text="Добавить", callback_data=f"add_expenses")]]
+    text = f"Категория {context.user_data.get('choosen_category_name')}, дата {date.strftime('%d.%m.%Y')}"
+    db.execute("SELECT name,amount FROM users_expenses  WHERE  user_category_id = %s and date = %s", (cat_id, date,))
+    exps = db.fetchall()
+    log.debug(f'date: {date}, cat_id: {cat_id}')
+    if exps:
+        text += '\n' + '\n'.join(
+            [f"{name} - {amount if int(amount) != amount else int(amount)}" for name, amount in exps])
+        markup.append([InlineKeyboardButton(text="Удалить", callback_data=f"del_expenses")])
+    else:
+        text += "\nСписок пуст!"
+    markup.append([InlineKeyboardButton(text="Назад", callback_data=f"add_expenses_back")])
+    markup = InlineKeyboardMarkup(markup)
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=text, reply_markup=markup)
+
+
+async def buttons(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    query = update.callback_query
+    await query.answer()
+    if query.data.startswith('tz_'):
+        timezone = query.data[3:]
+        await set_user_tz(update, context, timezone)
+    elif query.data.startswith('category_'):
+        category = query.data[9:]
+        context.user_data['action'] = 'choose_date'
+        context.user_data['choosen_category_id'] = category[:category.index('_')]
+        context.user_data['choosen_category_name'] = category[category.index('_') + 1:]
+        await enter_date_caption(update, context)
+    elif query.data == 'date_back':
+        context.user_data['action'] = 'choose_category'
+        context.user_data['choosen_category_id'] = 0
+        context.user_data['choosen_category_name'] = 0
+        await get_categories(update, context)
+    elif query.data == 'date_write':
+        context.user_data['action'] = 'write_date'
+        await write_date_caption(update, context)
+    elif query.data.startswith('date_'):
+        day = {'today': 0, 'yestarday': 1, 'preyestarday': 2}[query.data[5:]]
+        tz = await get_user_tz(update, context)
+        date = datetime.datetime.now(tz=pytz.timezone(tz)) - datetime.timedelta(days=day)
+        context.user_data['expenses_date'] = date.date()
+        await show_expenses(update, context)
+    elif query.data == 'add_expenses_back':
+        context.user_data['action'] = 'choose_date'
+        await enter_date_caption(update, context)
+    elif query.data == 'add_expenses':
+        context.user_data['action'] = 'write_expenses'
+        await add_expenses_caption(update, context)
+    elif query.data == 'write_expenses_back':
+        context.user_data['action'] = 'show_expenses'
+        await show_expenses(update, context)
+    elif query.data == 'del_expenses':
+        context.user_data['action'] = 'delete_expenses'
+        await delete_expenses_caption(update, context)
+    elif query.data.startswith('del_exp_'):
+        exp_id = query.data[8:]
+        db.execute("DELETE FROM users_expenses WHERE id = %s", (exp_id,))
+        await delete_expenses_caption(update, context)
+
+
+async def messages(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    # log.debug(f'message: {update.message}')
+    msg = update.message.text
+    if msg.startswith('/'):
+        await update.effective_message.reply_text("Такой команды не существует :(")
+        return
+    log.debug(f'message: {msg}, action: {context.user_data.get("action", 0)}')
+    if context.user_data.get('action') == 'setting_category':
+        context.user_data['setting_category'] = msg
+        await add_category(update, context)
+    elif context.user_data.get('action') == 'write_expenses':
+        context.user_data['writed_expenses'] = msg
+        await add_expenses(update, context)
+    elif context.user_data.get('action') == 'write_date':
+        context.user_data['writed_date'] = msg
+        await write_date(update, context)
+
+
+async def unknown_cmd(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.effective_message.reply_text("Такой команды не существует :(")
 
 
 if __name__ == "__main__":
     application = ApplicationBuilder().token(token).build()
 
-    start_handler = CommandHandler('start', start)
-    application.add_handler(start_handler)
+    application.add_handler(CommandHandler('start', start))
+    application.add_handler(CallbackQueryHandler(buttons))
+    application.add_handler(CommandHandler('category', get_categories))
+    application.add_handler(CommandHandler('total', get_total))
+    application.add_handler(CommandHandler('set_category', set_category))
+    application.add_handler(MessageHandler(filters.TEXT, messages))
+    # application.add_handler(MessageHandler(filters.COMMAND, unknown_cmd))
 
     application.run_polling()
-    # log.debug(f'start app')
-    # app.run(host="0.0.0.0",port=5000)
+
+# python-telegram-bot
+# python-dotenv
+# psycopg2
+# python-telegram-bot[job_queue]
---
+++ app/internal_functions.py
@@ -0,0 +1,120 @@
+from logger import create_logger
+from telegram import Update, Bot, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler, filters, \
+    CallbackQueryHandler, JobQueue
+from dotenv import load_dotenv, find_dotenv
+from pytz import country_timezones
+import datetime, time, logging, os, psycopg2, functools, requests, pytz
+
+log = create_logger('app.log', 'func', 10)
+load_dotenv(find_dotenv())
+db_connection = psycopg2.connect(
+    database=os.environ.get('database_name'),
+    user=os.environ.get('database_user'),
+    password=os.environ.get('database_password'),
+    host=os.environ.get('database_url'),
+    port=os.environ.get('database_port')
+)
+db = db_connection.cursor()
+
+
+async def get_user_db_id(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    user_db_id = context.user_data.get('user_db_id', 0)
+    if not user_db_id:
+        db.execute("SELECT telegram_id FROM users WHERE  telegram_id = %s", (update.effective_user.id,))
+        user_db_id = db.fetchone()
+        user_db_id = user_db_id[0] if user_db_id else 0
+        context.user_data['user_db_id'] = user_db_id
+    if not user_db_id:
+        first_name = update.effective_user.first_name if update.effective_user.first_name else ''
+        last_name = update.effective_user.last_name if update.effective_user.last_name else ''
+        username = update.effective_user.username if update.effective_user.username else ''
+        try:
+            db.execute("INSERT INTO users (telegram_id,first_name,last_name,username) VALUES (%s, %s, %s, %s)",
+                       (update.effective_user.id, first_name, last_name, username,))
+            db_connection.commit()
+            user_db_id = await get_user_db_id(update, context)
+        except:
+            await context.bot.send_message(chat_id=update.effective_chat.id, text="Не могу вас добавить :(")
+    return user_db_id
+
+
+async def get_user_tz(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    user_tz = context.user_data.get('user_tz', 0)
+    if not user_tz:
+        db.execute("SELECT timezone FROM users WHERE  telegram_id = %s", (update.effective_user.id,))
+        user_tz = db.fetchone()
+        user_tz = user_tz[0] if user_tz and user_tz[0] else 0
+        context.user_data['user_tz'] = user_tz
+    if not user_tz:
+        region = str(update.effective_user.language_code)
+        timezones = country_timezones(region)
+        keys = [[InlineKeyboardButton(text=f"{x}", callback_data=f"tz_{x}")] for x in timezones]
+        reply_markup = InlineKeyboardMarkup(keys)
+        text = "Для работы службы ежедневного оповещения о тратах, выберете ваш часовой пояс:"
+        await update.message.reply_text(text, reply_markup=reply_markup)
+    return user_tz
+
+
+async def set_user_tz(update: Update, context: ContextTypes.DEFAULT_TYPE, tz):
+    db.execute("UPDATE users SET timezone = %s WHERE telegram_id = %s", (tz, update.effective_user.id,))
+    db_connection.commit()
+    await get_user_tz(update, context)
+    now = datetime.datetime.now(tz=pytz.timezone(tz))
+    wd = {0: 'Воскресенье', 1: 'Понедельник', 2: 'Вторник', 3: 'Среда', 4: 'Четверг', 5: 'Пятница', 6: 'Суббота'}
+    cur = now.strftime('%d.%m.%Y') + f', {wd[int(now.strftime("%w"))]}, ' + now.strftime('%H:%M:%S')
+    text = f'Вы выбрали часовой пояс {tz}, текущие дата и время:  {cur}'
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=text)
+    await create_daily_job(update, context)
+
+
+async def create_daily_job(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    chat_id, user_id = update.effective_chat.id, update.effective_user.id
+    job_name = f'{user_id}_{chat_id}_{daily_sum}'
+    job_exist = context.job_queue.get_jobs_by_name(job_name)
+    tz = await get_user_tz(update, context)
+    if job_exist or not tz:
+        return
+    exec_time = datetime.time(hour=23, minute=59, tzinfo=pytz.timezone(tz))
+    context.job_queue.run_daily(daily_sum, exec_time, chat_id=chat_id, name=job_name, user_id=user_id)
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="Ежедневная задача создана.")
+
+
+async def daily_sum(context: ContextTypes.DEFAULT_TYPE):
+    tz = context.user_data.get('user_tz', 0)
+    date = datetime.datetime.now(tz=pytz.timezone(tz)).date()
+    db.execute(
+        "SELECT ue.amount,c.name FROM users_expenses as ue JOIN users_categories as uc ON ue.user_category_id=uc.id JOIN categories as c ON c.id=uc.category_id  WHERE  uc.user_id = %s and ue.date = %s",
+        (context.user_data.get('user_db_id'), date,))
+    exps = db.fetchall()
+    if exps:
+        exps_total = {}
+        total = 0
+        for amount, name in exps:
+            if name in exps_total:
+                exps_total[name] = exps_total[name] + amount
+            else:
+                exps_total.update({name: amount})
+            total += amount
+        text = f"{date.strftime('%Y-%m-%d')}\n"
+        for name, amount in exps_total.items():
+            text += f'{name}: {amount if int(amount)!=amount else int(amount)}\n'
+        text += f'Total: {total if int(total)!=total else int(total)}'
+    else:
+        text = "Сегодня у вас нет расходов."
+    await context.bot.send_message(chat_id=context.job.chat_id, text=text)
+
+
+def checking():
+    def wrapper(func):
+        @functools.wraps(func)
+        async def wrapped(*args):
+            await get_user_db_id(*args)
+            await get_user_tz(*args)
+            await create_daily_job(*args)
+            args[1].user_data['action']=0
+            return await func(*args)
+
+        return wrapped
+
+    return wrapper
